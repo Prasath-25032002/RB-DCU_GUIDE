21b52075f0526e6670365994a889e9b25a681277
bharati <bharati.k@phytecembedded.in>
2025-08-05 15:57:36 +0530

    Add new file to source/_static directory

--- source/conf.py
+++ source/conf.py
@@ -15,10 +15,12 @@ html_theme = 'sphinx_rtd_theme'
 
 html_static_path = ['_static']
 html_css_files = ['custom.css']
-
+html_js_files = [
+    'sidebar-toggle.js',
+]
 # Optional: for better navigation
 html_theme_options = {
-    'collapse_navigation': False,
+    'collapse_navigation': True,
     'navigation_depth': 4,
     'style_external_links': True,
 }
